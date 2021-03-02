67427bf8a026dec5bdf67d011babadc37161ce32
Cam Wills <cameron.wills89@outlook.com>
2021-03-02 12:45:07 -0500

    initial files

---
+++ client/src/components/About/AboutCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "../../style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
+            from <span className="purple"> Bhubaneswar, India.</span>
+            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
+            Mesra.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <i className="far fa-hand-point-right"></i> Playing Games
+            </li>
+            <li className="about-activity">
+              <i className="far fa-hand-point-right"></i> Writting Tech Blogs
+            </li>
+            <li className="about-activity">
+              <i className="far fa-hand-point-right"></i> Travelling
+            </li>
+          </ul>
+
+          <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
