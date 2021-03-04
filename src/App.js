0d773ccbde4cb83faa29b496584da6966059d3aa
Cam Wills <cameron.wills89@outlook.com>
2021-03-04 15:30:07 -0500

    fixed page load error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
+import Preloader from "./components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
